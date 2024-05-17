7ecc09bb653406b2308f43ca751a3ad108670c7b
Caleb Curran <calebcurran.velasco@gmail.com>
2024-05-17 11:11:51 -0600

    changed the protocol to UDP instead of TCP

--- Assets/Scenes/CameraStream.cs
+++ Assets/Scenes/CameraStream.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
@@ -13,25 +14,26 @@ public class CameraStream : MonoBehaviour
     public string camIP = "127.0.0.1";
     public int camPort = 8081;
     private Texture2D camTexture; // Texture to load the image
-    private TcpClient camClient;
+    private UdpClient udpClient;
+    private IPEndPoint endPoint;
     private bool connectCam = true;
 
     void Start()
     {
         camTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        StartCoroutine(GetVideoStream());
+        udpClient = new UdpClient();
+        endPoint = new IPEndPoint(IPAddress.Parse(camIP), camPort);
+        StartCoroutine(SendVideoStream());
     }
 
-    IEnumerator GetVideoStream()
+    IEnumerator SendVideoStream()
     {
         while (connectCam)
         {
             yield return new WaitForSeconds(1); // wait for one second between frames
+
             try
             {
-                camClient = new TcpClient(camIP, camPort);
-                NetworkStream dataStream = camClient.GetStream();
-
                 // Capture the camera image
                 RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
                 cam.targetTexture = renderTexture;
@@ -45,23 +47,19 @@ public class CameraStream : MonoBehaviour
                 Destroy(renderTexture);
 
                 byte[] byteArray = camTexture.EncodeToJPG();
-                int fileSize = byteArray.Length;
-
-                // Send the file size header
-                byte[] header = Encoding.ASCII.GetBytes(fileSize.ToString("D10")); // Ensure header is always 10 bytes
-                dataStream.Write(header, 0, header.Length);
 
                 // Send the image data
-                dataStream.Write(byteArray, 0, byteArray.Length);
-
-                // Close the connection
-                dataStream.Close();
-                camClient.Close();
+                udpClient.Send(byteArray, byteArray.Length, endPoint);
             }
             catch (Exception e)
             {
                 Debug.LogError(e);
             }
         }
+    }
+
+    void OnApplicationQuit()
+    {
+        udpClient.Close();
     }
 }
