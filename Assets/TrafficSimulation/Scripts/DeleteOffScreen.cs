57960c254f04d4ba5a527975775041370c538075
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-05-17 11:20:58 -0600

    improved destructor mechanism

--- Assets/TrafficSimulation/Scripts/DeleteOffScreen.cs
+++ Assets/TrafficSimulation/Scripts/DeleteOffScreen.cs
@@ -1,28 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeleteOffScreen : MonoBehaviour
 {
-    public Vector2 widthThresold;
-    public Vector2 heightThresold;
-    // Start is called before the first frame update
-    void Start()
-    {
-        widthThresold = new Vector2(-0.1f, Screen.width * 1.04f);
-        heightThresold = new Vector2(-0.1f, Screen.height * 1.04f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-        if (screenPosition.x < widthThresold.x || 
-            screenPosition.y < 0 ||
-            screenPosition.x > widthThresold.y || 
-            screenPosition.y > heightThresold.y)
-        {
-            Destroy(gameObject);
+    public void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.tag == "Destructor" ){
+            Destroy(this.gameObject);
         }
     }
 }
