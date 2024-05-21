8121ca8d2b39352aba188faf108574822fa40526
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-05-21 10:59:31 -0600

    polished city

--- Assets/TrafficSimulation/Scripts/SpawnManagerRight.cs
+++ Assets/TrafficSimulation/Scripts/SpawnManagerRight.cs
@@ -15,7 +15,7 @@ public class SpawnManagerRight : MonoBehaviour
         targetTime -= Time.deltaTime;        
 
         if(targetTime <= 0){
-            targetTime = Random.Range(8, 15); // set new timer for 8 to 15 sec
+            targetTime = Random.Range(10, 16); // set new timer for 10 to 16 sec
 
             GameObject newCarGameObject = Instantiate(carPrefabs[Random.Range(0, carPrefabs.Length)], transform.position, Quaternion.identity);
             newCarGameObject.transform.Rotate(1, 90, 1);
