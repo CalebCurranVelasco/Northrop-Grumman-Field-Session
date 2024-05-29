0cce3d6ad8b15dad06751ba768d325240e6fd9d8
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-05-29 17:17:56 -0600

    added boards and changed camera angles

---
+++ Assets/PrintSize.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.GetComponent<Renderer>().bounds.size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
