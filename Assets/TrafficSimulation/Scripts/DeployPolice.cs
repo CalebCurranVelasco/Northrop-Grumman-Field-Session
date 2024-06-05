0fa067927a3614f403ba84930f01a85cc3227e89
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-06-04 21:15:32 -0600

    restart after unity crash

--- Assets/TrafficSimulation/Scripts/DeployPolice.cs
+++ Assets/TrafficSimulation/Scripts/DeployPolice.cs
@@ -6,18 +6,17 @@ using UnityEngine;
 namespace TrafficSimulation{
     public class DeployPolice : MonoBehaviour
         {
-        public VehicleAI robberVehicleAI; // Reference to a VehicleAI instance
-        [Tooltip("Add robber vehicle to search for")]
-        public GameObject robberVehicle; 
-        public Vector3 robberLoc = Vector3.zero;
+        // [Tooltip("Add robber vehicle to search for")]
+        // public GameObject robberVehicle; 
+        public Vector3[] robberLoc = {Vector3.zero, Vector3.zero};
         public Vector3[] policeTargets;
 
         private TrafficSystem trafficSystem; 
         public List<Segment> segments;
         void Start()
         {
-            // Example of finding the VehicleAI component on the same GameObject
-            robberVehicleAI = robberVehicle.GetComponent<VehicleAI>();
+            trafficSystem = GetComponent<TrafficSystem>();
+            // segments = trafficSystem.getSegments();
         }
 
         // Update is called once per frame
@@ -26,36 +25,35 @@ namespace TrafficSimulation{
             // socket listens for robber position
 
             // if socket recieves coordinates of robber
-            if (robberLoc != Vector3.zero){
+            if (robberLoc[0] != Vector3.zero){
                 // find robber's current and target segement
-                trafficSystem = GetComponent<TrafficSystem>();
-                segments = trafficSystem.getSegments();
-
-                foreach(Segment segment in segments){
-                    //Find nearest waypoint 
-                    float closestWaypoint  = float.MaxValue;
-
-                    foreach(Waypoint waypoint in segments[segment].waypoints){
-                        Vector3 waypointLoc = Camera.main.WorldToScreenPoint(waypoint.transform.position);
-                        
-                        if(manhattanDist < closestSegDist){
-                            closestSegDist = manhattanDist;
-                            closestSeg = nextSeg;
-                        }
-            
-
-
-                        float d = Vector3.Distance(this.transform.position, trafficSystem.segments[currentTarget.segment].waypoints[j].transform.position);
-                        //Only take in front points
-                        Vector3 lSpace = this.transform.InverseTransformPoint(trafficSystem.segments[currentTarget.segment].waypoints[j].transform.position);
-                        if(d < minDist && lSpace.z > 0){
-                            minDist = d;
-                            currentTarget.waypoint = j;
-                        }
-                    }
-                }                
+                // getRobberTargetWaypoint();
             }
         }
+
+        // Vector3 getRobberTargetWaypoint()
+        // {
+        //     Vector3 closestWaypointDist  = float.MaxValue;
+        //     Vector3 closestWaypoint; 
+
+        //     // foreach(Segment segment in segments){
+        //     //     foreach(Waypoint waypoint in segments[segment].waypoints){
+        //     //         Vector3 waypointLoc = Camera.main.WorldToScreenPoint(waypoint.transform.position);
+
+        //     //         if(robberLoc[1] == Vector3.zero){ // if no direction, return the closest waypoint
+        //     //             float manhattanDist = Math.Abs(waypointLoc.x - robberLoc[0].x) + Math.Abs(waypointLoc.z - robberLoc[0].z);
+
+        //     //             if(manhattanDist < closestWaypointDist){
+        //     //                 closestWaypointDist = manhattanDist;
+        //     //                 closestWaypointLoc = waypointLoc;
+        //     //             } 
+        //     //         } else{ // return closest waypoint in direction of travel
+
+        //     //         }
+        //     //     }
+        //     // }
+        //     return closestWaypointDist;
+        // }
     }
 }
 
