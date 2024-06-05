fb2439267429078d76e2d164991d0bb36dd619f8
Madelyn Swelstad <madelynswelstad@gmail.com>
2024-06-04 22:17:06 -0600

    calculates closest waypoint in the direction of travel

--- Assets/TrafficSimulation/Scripts/DeployPolice.cs
+++ Assets/TrafficSimulation/Scripts/DeployPolice.cs
@@ -46,4 +46,39 @@ namespace TrafficSimulation{
                     float euclideanDistance = Math.Abs(waypointPosition.x - robberLoc[0].x) + Math.Abs(waypointPosition.z - robberLoc[0].z);
 
                     if(euclideanDistance < closestWaypointDelta){
-                        if(robberLoc[1] == Vector3.zero){ // if no direction, return the closest 
+                        if(robberLoc[1] == Vector3.zero){ // if no direction, return the closest waypoint
+                            closestWaypointDelta = euclideanDistance;
+                            closestWaypointPosition = waypointPosition;
+                        } 
+                        // return closest waypoint in direction of travel
+                        else{ 
+                            // if robber is moving right and closest waypoint is to the right
+                            if(robberLoc[0].x - robberLoc[1].x > deltaThreshold && closestWaypointPosition.x - robberLoc[1].x > 0){
+                                closestWaypointDelta = euclideanDistance;
+                                closestWaypointPosition = waypointPosition;
+                            } 
+                            // if robber is moving left and closeest waypoint is to the left
+                            else if(robberLoc[0].x - robberLoc[1].x < deltaThreshold && closestWaypointPosition.x - robberLoc[1].x < 0){
+                                closestWaypointDelta = euclideanDistance;
+                                closestWaypointPosition = waypointPosition;
+                            } 
+                            // if robber is moving up and closest waypoint is up
+                            else if(robberLoc[0].z - robberLoc[1].z > deltaThreshold && closestWaypointPosition.z - robberLoc[1].z > 0){
+                                closestWaypointDelta = euclideanDistance;
+                                closestWaypointPosition = waypointPosition;
+                            } 
+                            // if robber is moving down and closest waypoint is down
+                            else if(robberLoc[0].z - robberLoc[1].z < deltaThreshold && closestWaypointPosition.z - robberLoc[1].z < 0){
+                                closestWaypointDelta = euclideanDistance;
+                                closestWaypointPosition = waypointPosition;
+                            }
+                            // otherwise robber is moving away from this waypoint and it should not be considered closest to the robber
+                        }
+                    }
+                }
+            }
+            return closestWaypointPosition;
+        }
+    }
+}
+
